e43fe4ef195cda2673412322093924a532eff442
bikramgiri <asgbrg.github@gmail.com>
2025-01-31 14:11:27 +0545

    Add Comment

--- src/App.jsx
+++ src/App.jsx
@@ -40,16 +40,17 @@
 
 
 
-// import './App.css'
+// import './App.css' 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Home'
 
+ // Create Home, About, and Contact components.
 function App() {
 
   return (
     <BrowserRouter> 
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      <Route path="/" element={<Home/>}/>  
       <Route path="/about" element={<h2>About Page</h2>}/>
       <Route path="/contact" element={<h1>Contact Page</h1>}/>
     </Routes> 
